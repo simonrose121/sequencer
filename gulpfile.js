2604101047b160b706301e5b25eaf0d8cd48db8e
Simon Rose <simonrose121@gmail.com>
2017-01-06 17:09:18 +0000

    Use less features in existing css

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
 var path = require('path');
-var gutil = require('gulp-util');
 
 gulp.task('less', function() {
-    return gulp.src('')
+    return gulp.src('app/*.less')
         .pipe(watch('app/*.less'))
         .pipe(less())
         .pipe(gulp.dest('app'));
