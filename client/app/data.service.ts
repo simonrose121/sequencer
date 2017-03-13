5f587c217939ac20840a6976c58b56fcac5c8a5a
Simon Rose <simonrose121@gmail.com>
2017-03-13 15:07:30 +0000

    Parameter types on data service

--- client/app/data.service.ts
+++ client/app/data.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 
 import { UtilitiesService } from './utilities.service';
 
+import { Story } from './../models/story';
+
 @Injectable()
 export class DataService {
     constructor(private utilitiesService: UtilitiesService) {}
 
     private id = 1; // TODO: Change me back
 
-    setId(id) {
+    setId(id : number) {
         this.id = id;
     }
 
-    storeMark(story, mark) {
+    storeMark(story : Story, mark : number) {
         console.log('---------------');
         console.log('user: ' + this.id);
         console.log('question: ' + story.action);
