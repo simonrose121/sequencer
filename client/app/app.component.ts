26bcf9b1ae947c986a87c95cf55be176c478fa78
Simon Rose <simonrose121@gmail.com>
2017-04-03 10:08:05 +0100

    Add start button with countdown

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -10,12 +10,30 @@ import { LogService } from './log.service';
     ]
 })
 export class AppComponent { 
-    id;
+    id : Number;
+    startScreen : boolean;
+    countdownText : string;
 
-    constructor(private logService: LogService) {}
+    constructor(private logService: LogService) {
+        this.countdownText = 'Start';
+    }
 
     setId(id) {
         this.id = id;
         this.logService.setId(id);
+        this.startScreen = true;
+    }
+
+    countdown() {
+        this.countdownText = '3';
+        setTimeout(() => {
+            this.countdownText = '2';
+        }, 1000);
+        setTimeout(() => {
+            this.countdownText = '1';
+        }, 2000);
+        setTimeout(() => {
+            this.startScreen = false;
+        }, 3000);
     }
 }
