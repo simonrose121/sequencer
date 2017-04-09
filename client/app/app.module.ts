9f421380200273feffc8fba0fe9f7a6520ece8ec
Simon Rose <simonrose121@gmail.com>
2017-04-09 14:13:01 +0100

    Create scores component

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,23 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question.component';
+import { ScoresComponent } from './scores.component';
 import { StoryService } from './story.service';
 import { LogService } from './log.service';
 import { UtilitiesService } from './utilities.service';
 import { ConfigService } from './config.service';
 
+const appRoutes: Routes = [
+    { path: 'quiz', component: QuestionComponent },
+    { path: 'scores', component: ScoresComponent }
+]
+
 @NgModule({
     imports: [
         BrowserModule,
         HttpModule,
-        SlimLoadingBarModule.forRoot()
+        SlimLoadingBarModule.forRoot(),
+        RouterModule.forRoot(appRoutes)
     ],
     declarations: [
         AppComponent,
-        QuestionComponent
+        QuestionComponent,
+        ScoresComponent
     ],
     providers: [
         StoryService,
---
+++ client/app/scores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'score',
+    templateUrl: 'app/scores.component.html',
+    styleUrls: [
+        'app/scores.component.css',
+    ]
+})
+export class ScoresComponent {
+    
+}
