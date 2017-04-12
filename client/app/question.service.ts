ca0401752e81905a84eefed0fb8ae85f20b6091e
Simon Rose <simonrose121@gmail.com>
2017-04-12 10:08:11 +0100

    Remove unused references from question service

--- client/app/question.service.ts
+++ client/app/question.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
 
 import { HttpService } from "./http.service";
-
-import { Card } from './models/card';
-import { Question } from './models/question';
 
 @Injectable()
 export class QuestionService {
